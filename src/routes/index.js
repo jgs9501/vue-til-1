d140abda1c11e177372102e5a0b7a43282f2839d
junsoo <jgs9501@naver.com>
2022-03-15 16:27:34 +0900

    라우터 설정

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,12 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 export default new VueRouter({
+  // 배포를 할 경우 vuejs.org history 검색해보면 정적인 자원들을 서버에 배포 할 때,
+  // 서버에 url에 대한 필터링 기능을 넣어주어야 한다
+  mode: 'history',
   routes: [
     {
+      // 모든 패스는 로그인으로 리다이렉트
       path: '/',
       redirect: '/login',
     },
@@ -18,6 +22,11 @@ export default new VueRouter({
     {
       path: '/signup',
       component: () => import('@/views/SignupPage.vue'),
+    },
+    {
+      // 위쪽 글을 제외하나 나머지 URL이 해당
+      path: '*',
+      component: () => import('@/views/NotFoundPage.vue'),
     },
   ],
 });
---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Page is Not found
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
