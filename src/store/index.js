20f1884a1d30272dab22f2b9b7b377030c657c3f
junsoo <jgs9501@naver.com>
2022-05-01 22:05:39 +0900

    라우터 심화

--- src/store/index.js
+++ src/store/index.js
@@ -30,11 +30,13 @@ export default new Vuex.Store({
     setToken(state, token) {
       state.token = token;
     },
+    clearToken(state) {
+      state.token = '';
+    },
   },
   actions: {
     async LOGIN({ commit }, userData) {
       const { data } = await loginUser(userData);
-      console.log(data.token);
       commit('setToken', data.token);
       commit('setUsername', data.user.username);
       saveAuthToCookie(data.token);
